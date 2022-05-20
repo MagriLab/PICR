d5a1fe34a9433f3baf48ee01338a150fa849811d
danielkelshaw <daniel.j.kelshaw@gmail.com>
2022-05-21 00:01:44 +0100

    Removed R(u) from g(u, \phi) loss term

--- picr/solvers/torch.py
+++ picr/solvers/torch.py
@@ -70,31 +70,6 @@ class LinearCDS(KolSol):
 
         return dudt
 
-    @staticmethod
-    def g_u_phi(u_hat: torch.Tensor, phi_hat: torch.Tensor) -> torch.Tensor:
-
-        """Calculate g(u, \phi) for the linear convection diffusion equations.
-
-        Parameters
-        ----------
-        u_hat: torch.Tensor
-            Predicted velocity field in the Fourier domain.
-        phi_hat: torch.Tensor
-            Predicted corruption field in the Fourier domain.
-
-        Returns
-        -------
-        guphi: torch.Tensor
-            g(u, \phi) for the linear convection diffusion equations.
-        """
-
-        if not u_hat.shape == phi_hat.shape:
-            raise DimensionError(msg=f'u_hat.shape ({u_hat.shape}) should match phi_hat.shape ({phi_hat.shape})')
-
-        guphi = torch.zeros_like(phi_hat)
-
-        return guphi
-
 
 class NonlinearCDS(KolSol):
 
@@ -154,63 +129,3 @@ class NonlinearCDS(KolSol):
         dudt = -u_dot_nabla_u - laplace_u
 
         return dudt
-
-    def g_u_phi(self, u_hat: torch.Tensor, phi_hat: torch.Tensor, dt: float) -> torch.Tensor:
-
-        """Calculate g(u, \phi) for the nonlinear convection diffusion equations.
-
-        Parameters
-        ----------
-        u_hat: torch.Tensor
-            Predicted velocity field in the Fourier domain.
-        phi_hat: torch.Tensor
-            Predicted corruption field in the Fourier domain.
-        dt: float
-            Time-step of the simulation.
-
-        Returns
-        -------
-        guphi: torch.Tensor
-            g(u, \phi) for the nonlinear convection diffusion equations.
-        """
-
-        # calculate u \cdot \nabla \phi, \phi \cdot \nabla u
-        uij_aapt = []
-        for u_i in range(self.ndim):
-
-            uj_aapt = []
-            for u_j in range(self.ndim):
-                uj_aapt.append(self.aap(u_hat[..., u_j], phi_hat[..., u_i]))
-
-            uij_aapt.append(torch.stack(uj_aapt, dim=0))
-
-        aapt = torch.stack(uij_aapt, dim=0)
-
-        u_dot_nabla_phi = oe.contract('...t, ut... -> ...u', self.nabla, aapt)
-        phi_dot_nabla_u = oe.contract('...t, tu... -> ...u', self.nabla, aapt)
-
-        # calculate u \cdot \nabla \u
-        uij_aapt = []
-        for u_i in range(self.ndim):
-
-            uj_aapt = []
-            for u_j in range(self.ndim):
-                uj_aapt.append(self.aap(u_hat[..., u_j], u_hat[..., u_i]))
-
-            uij_aapt.append(torch.stack(uj_aapt, dim=0))
-
-        aapt = torch.stack(uij_aapt, dim=0)
-
-        u_dot_nabla_u = oe.contract('...t, ut... -> ...u', self.nabla, aapt)
-        laplacian_u = self.nu * oe.contract('..., ...u -> ...u', self.kk, u_hat)
-
-        # time derivative and slice
-        du_dt = (1.0 / dt) * (u_hat[:, 1:, ...] - u_hat[:, :-1, ...])
-        u_dot_nabla_u = u_dot_nabla_u[:, :-1, ...]
-        u_dot_nabla_phi = u_dot_nabla_phi[:, :-1, ...]
-        phi_dot_nabla_u = phi_dot_nabla_u[:, :-1, ...]
-        laplacian_u = laplacian_u[:, :-1, ...]
-
-        guphi = du_dt + u_dot_nabla_u + u_dot_nabla_phi + phi_dot_nabla_u + laplacian_u
-
-        return guphi
